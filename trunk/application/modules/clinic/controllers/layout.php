3733d8225081f3d21a3db35513917f4cc98813f7
pvgiang106@gmail.com <pvgiang106@gmail.com@93b31c22-8e38-b0b7-556f-51acfc4263ca>
2014-05-13 09:36:40 +0000

    Edit view clinic

--- trunk/application/modules/clinic/controllers/layout.php
+++ trunk/application/modules/clinic/controllers/layout.php
@@ -16,8 +16,13 @@ class Layout extends MX_Controller {
     public function render($data) {
         // render view
         $data['tab'] = ($this->session->userdata('tab') == null) ? 0 : $this->session->userdata('tab');
-        $this->load->helper('third_library');             
-        $this->load->view('clinic/view_layout', $data);
+        $this->load->helper('third_library');
+		if($data['view_file'] == 'view_appointment'){
+			$this->load->view('clinic/view_appointment',$data);
+		}
+		else{
+			$this->load->view('clinic/view_layout', $data);
+		}
     }
 }
 
--- trunk/application/modules/clinic/views/view_appointment.php
+++ trunk/application/modules/clinic/views/view_appointment.php
@@ -1,6 +1,25 @@
-<?
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Access the desktop camera and video using HTML, JavaScript, and Canvas.  The camera may be controlled using HTML5 and getUserMedia." />
+	<script type="text/javascript" src="<?php echo base_url('/assets/systemfile/plugin/dhtmlxscheduler/dhtmlxscheduler.js'); ?>" ></script>
+	<link rel="stylesheet" href="<?php echo base_url('/assets/systemfile/plugin/dhtmlxscheduler/dhtmlxscheduler.css'); ?>" />
 
-?>
+	<style>
+		body{
+			margin:auto;
+			padding:0px;
+			height:100%;
+			width:1170px;
+			height:800px;
+			overflow:hidden;
+			background: #EBF3EC;
+		}
+		</style>
+</head>
+<body>
 <div id="scheduler_here" class="dhx_cal_container" style='width:100%; height:100%;'>
     <div class="dhx_cal_navline">
         <div class="dhx_cal_prev_button">&nbsp;</div>
@@ -14,6 +33,9 @@
     <div class="dhx_cal_header"></div>
     <div class="dhx_cal_data"></div>       
 </div>
-<script>
-	scheduler.init('scheduler_here', new Date(),"month");
-</script>
+	<script>
+		scheduler.init('scheduler_here', new Date(),"month");
+	</script>
+</body>
+</html>
+

39124f1462f983c8250b91e3cb503ebf32b2dc8c
pvgiang106@gmail.com <pvgiang106@gmail.com@93b31c22-8e38-b0b7-556f-51acfc4263ca>
2014-05-13 09:36:40 +0000

    Edit view clinic

--- application/modules/clinic/views/view_layout.php
+++ application/modules/clinic/views/view_layout.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Access the desktop camera and video using HTML, JavaScript, and Canvas.  The camera may be controlled using HTML5 and getUserMedia." />        <!--bootstrap-->
         <?php
-        //echo loadBootstrap3_css();
+        echo loadBootstrap3_css();
         ?>
         <!-- bootstrap widget theme -->
         <link rel="stylesheet" href="<?php echo base_url('/assets/systemfile/css/theme.bootstrap.css'); ?>" />
@@ -34,12 +34,20 @@
 <!-- add on pager plugin tablesorter -->
 <script type="text/javascript" src="<?php echo base_url('/assets/systemfile/plugin/tablesorter/js/jquery.tablesorter.pager.js');?>"></script>
     </head>
-    <body >
-
+    <body class="container">
+        <div class="row">
+            <ul class="nav nav-pills">
+                <li <?php if($tab == 0) echo "class='active'"; ?>><a href="<?php echo base_url(); ?>clinic">Appointment</a></li>
+                <li <?php if($tab == 1) echo "class='active'"; ?>><a href="<?php echo base_url(); ?>index.php/clinic/medicalprofile">Medical Profile</a></li>
+                <li <?php if($tab == 2) echo "class='active'"; ?>><a href="<?php echo base_url(); ?>index.php/clinic/setuptime">Setup Time</a></li>
+                <li <?php if($tab == 3) echo "class='active'"; ?>><a href="<?php echo base_url(); ?>index.php/clinic/waitingappointment">Waitingappointment</a></li>
+            </ul>
+        </div>
+        <div class="row">
             <?php
             $this->load->view($module . '/' . $view_file);
             ?>
-
+        </div>
         <script type="text/javascript">
             $(document).ready(function() {
                  $("table").tablesorter().tablesorterPager({container: $(".pager")});    
